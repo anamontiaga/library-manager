18113af5e7e5fe1853261e83e71d037e080aa76c
Ana Montiaga <amontiaga@gmail.com>
2021-02-07 11:53:02 +0100

    feat: logout :sparkles:

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -5,7 +5,9 @@ const MY_AUTH_APP = 'MY_AUTH_APP'
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(window.localStorage.getItem(MY_AUTH_APP))
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    window.localStorage.getItem(MY_AUTH_APP)
+  )
 
   const login = useCallback(() => {
     window.localStorage.setItem(MY_AUTH_APP, true)
