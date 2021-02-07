0a7c1f5ef4cd5567f4ef0378ad817661aa67ff27
Ana Montiaga <amontiaga@gmail.com>
2021-02-07 13:34:34 +0100

    feat: add initialised animation :dizzy:

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { animated, useSpring } from 'react-spring'
 import { useAuthContext } from '../../../hooks/useAuthContext'
 import { MAGIC_WORD } from '../../constants/magic_word'
 import { SwitchThemeButton } from '../SwitchThemeButton'
@@ -7,6 +8,7 @@ import { ContainerEl, LoginButtonEl } from './style'
 export const Home = () => {
   const { login } = useAuthContext()
   const [magicWord, setMagicWord] = useState('')
+  const [initialised, setInitialised] = useState(false)
 
   const handleInputChange = (e) => {
     setMagicWord(e.target.value)
@@ -18,13 +20,25 @@ export const Home = () => {
     }
   }
 
+  useEffect(() => setInitialised(true))
+
+  const startFormAnimation = useSpring({
+    transform: initialised ? 'translateY(0)' : 'translateY(100vh)',
+    opacity: initialised ? 1 : 0,
+    transition: '2s'
+  });
+
+
   return (
     <ContainerEl>
       <SwitchThemeButton />
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={magicWord} onChange={handleInputChange} />
-        <LoginButtonEl type="submit">Iniciar sesión</LoginButtonEl>
-      </form>
+      <animated.div
+        style={startFormAnimation}>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={magicWord} onChange={handleInputChange} />
+          <LoginButtonEl type="submit">Iniciar sesión</LoginButtonEl>
+        </form>
+      </animated.div>
     </ContainerEl>
   )
 }
